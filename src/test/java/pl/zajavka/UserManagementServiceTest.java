617aa05bfa350d1b942789aa8facb5dbf787044c
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-04-04 12:51:10 +0200

    added some test methods

--- src/test/java/pl/zajavka/UserManagementServiceTest.java
+++ src/test/java/pl/zajavka/UserManagementServiceTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class UserManagementServiceTest {
 
     private UserManagementService userManagementService;
@@ -55,6 +59,48 @@ public class UserManagementServiceTest {
         Assertions.assertEquals(user3, result3.get());
 
     }
+
+    @Test
+    void shouldFailWhenDuplicateUserIsCreated() {
+        //given
+        var user1 = someUser();
+        var user2 = someUser();
+
+        // when, then
+        userManagementService.create(user1);
+        Throwable exception = Assertions.assertThrows(RuntimeException.class, () -> userManagementService.create(user2));
+        Assertions.assertEquals(
+                String.format("User with email: [%s] is already created", user1.getEmail()),
+                exception.getMessage());
+    }
+
+    @Test
+    void shouldFindUserWithName() {
+        //given
+        var user1 = someUser().withEmail("email1@gmail.com");
+        var user2 = someUser().withEmail("email1@gmail.com");
+        var user3 = someUser().withName("newName").withEmail("email1@gmail.com");
+
+        //when
+        userManagementService.create(user1);
+        userManagementService.create(user2);
+        userManagementService.create(user3);
+
+        var result1 = userManagementService.findByName(user1.getName());
+        var result2 = userManagementService.findByName(user2.getName());
+        var result3 = userManagementService.findByName(user3.getName());
+        var all = userManagementService.findAll();
+
+        //then
+        Assertions.assertEquals(3, all.size());
+        Assertions.assertEquals(
+                Stream.of(user1, user2).sorted().collect(Collectors.toList()),
+                result2.stream().sorted().collect(Collectors.toList())
+        );
+        Assertions.assertEquals(List.of(user3), result3);
+    }
+
+
 
 
 }
